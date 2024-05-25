1999f21387c07a4de31627701d6df1584b21f54c
msg-bq <82528553+msg-bq@users.noreply.github.com>
2024-05-26 07:36:52 +0800

    complete

--- train/self_train.py
+++ train/self_train.py
@@ -20,17 +20,17 @@ class SelfTrainingArguments(TrainingArguments):
         self.selftrain_iteration = kwargs.get("selftrain_iteration", 3) # self train的epoch
         self.selftrain_topk = kwargs.get("selftrain_topk", 4)# 这个值用于决定每轮获取软标注的topk
 
-class CustomLoss(nn.Module):
-    def __init__(self, weight=None, size_average=None, reduce=None, reduction='mean'):
-        super(CustomLoss, self).__init__()
-        # 定义你的损失函数所需的参数
-
-    def forward(self, prediction, target):
-        # 定义损失函数的计算方式
-        # prediction 是模型的预测结果
-        # target 是真实标签
-        loss = ...  # 根据需要计算损失值的逻辑
-        return loss
+# class CustomLoss(nn.Module):
+#     def __init__(self, weight=None, size_average=None, reduce=None, reduction='mean'):
+#         super(CustomLoss, self).__init__()
+#         # 定义你的损失函数所需的参数
+# 
+#     def forward(self, prediction, target):
+#         # 定义损失函数的计算方式
+#         # prediction 是模型的预测结果
+#         # target 是真实标签
+#         loss = ...  # 根据需要计算损失值的逻辑
+#         return loss
 
 
 class SelfTrainTrainer(Trainer):
@@ -39,16 +39,16 @@ class SelfTrainTrainer(Trainer):
         self.unlabeled_dataset = kwargs.get("unlabeled_dataset") # 这里不需要dataloader，只要一个dataset就行
         self.args = kwargs.get("args")
 
-    def softmax_chunks(self, lst, chunk_size=4):
-        """Apply softmax to chunks of size chunk_size in lst."""
-        # Convert list to numpy array
-        arr = np.array(lst)
-        # Reshape the array to have chunks of size chunk_size
-        chunked_arr = arr.reshape(-1, chunk_size)
-        # Apply softmax to each chunk
-        softmaxed_chunks = np.apply_along_axis(F.softmax, 1, chunked_arr)
-        # Flatten the result back to a 1D array if desired
-        return softmaxed_chunks.flatten() if chunk_size == 1 else softmaxed_chunks
+    # def softmax_chunks(self, lst, chunk_size=4):
+    #     """Apply softmax to chunks of size chunk_size in lst."""
+    #     # Convert list to numpy array
+    #     arr = np.array(lst)
+    #     # Reshape the array to have chunks of size chunk_size
+    #     chunked_arr = arr.reshape(-1, chunk_size)
+    #     # Apply softmax to each chunk
+    #     softmaxed_chunks = np.apply_along_axis(F.softmax, 1, chunked_arr)
+    #     # Flatten the result back to a 1D array if desired
+    #     return softmaxed_chunks.flatten() if chunk_size == 1 else softmaxed_chunks
 
     def get_beam_search_score(self, question) -> dict:
         input_ids = self.tokenizer(question, return_tensors="pt").input_ids \
@@ -118,19 +118,21 @@ class SelfTrainTrainer(Trainer):
                 example.score /= sum_scores
 
     def compute_loss(self, model, inputs, return_outputs=False):  ## compute loss这个步骤实际上定义了 forward和loss的计算过程
-        labels = inputs.get("labels")
-        outputs = model(inputs.get('inputs'))  ##在这里定义了foward和batch的计算过程
-        logits = outputs  # .get("logits")
-        # compute custom loss (suppose one has 3 labels with different weights)
-        loss_fct = nn.BCEWithLogitsLoss(reduction="mean")  ## loss可以不断重新定义无所谓的
-        loss = loss_fct(logits.view(-1, 1), labels.float().view(-1, 1))
-        # final_loss = torch.masked_select(loss, labels.view(-1, 1) != -1).mean()
-        # return (torch.masked_select(loss, labels.view(-1, 1) != -1).mean(), outputs) if return_outputs else loss
-        return (
-        loss, {'outputs': outputs}) if return_outputs else loss  # 一定要记得 compute loss的时候！！！outputs要用字典返回
+        score = torch.tensor(inputs.get("weight"))
+        return score * super(SelfTrainTrainer, self).compute_loss(model, inputs, return_outputs)
+        # labels = inputs.get("labels")
+        # outputs = model(inputs.get('inputs'))  ##在这里定义了foward和batch的计算过程
+        # logits = outputs  # .get("logits")
+        # # compute custom loss (suppose one has 3 labels with different weights)
+        # loss_fct = nn.BCEWithLogitsLoss(reduction="mean")  ## loss可以不断重新定义无所谓的
+        # loss = loss_fct(logits.view(-1, 1), labels.float().view(-1, 1))
+        # # final_loss = torch.masked_select(loss, labels.view(-1, 1) != -1).mean()
+        # # return (torch.masked_select(loss, labels.view(-1, 1) != -1).mean(), outputs) if return_outputs else loss
+        # return (
+        # loss, {'outputs': outputs}) if return_outputs else loss  # 一定要记得 compute loss的时候！！！outputs要用字典返回
 
 
-def train_model_self_train(model, optimizer, dataset, args):
+def train_model_self_train(model, tokenizer, optimizer, dataset, args):
     """
     1. 先训练基础模型
     2. 用基础模型预测unlabeled数据集
@@ -139,34 +141,48 @@ def train_model_self_train(model, optimizer, dataset, args):
     5. 重复2-4
     """
 
-    # 1. 先训练基础模型
-    train_args = TrainingArguments(output_dir=args.save_dir,
-                                   num_train_epochs=args.epoch, # 这个指每个self_train里面的epoch
-                                   per_device_train_batch_size=args.batch_size,
-                                   save_steps=1000,
-                                   learning_rate=args.lr,
-                                   evaluation_strategy="epoch")
+    for _ in range(3):
+        # 1. 先训练基础模型
+        train_args = TrainingArguments(output_dir=args.save_dir,
+                                       num_train_epochs=args.epoch, # 这个指每个self_train里面的epoch
+                                       per_device_train_batch_size=args.batch_size,
+                                       save_steps=1000,
+                                       learning_rate=args.lr,
+                                       evaluation_strategy="epoch")
 
-    trainer = Trainer(model=model,
-                      args=train_args,
-                      data_collator=mycollate, # 要么给自己的，要么在定义trainer后面单独写一个data_collator=None，不然代码里有默认collate
-                      train_dataset=dataset["train"],
-                      eval_dataset=dataset["eval"],
-                      tokenizer=model.tokenizer,
-                      optimizers=(optimizer, None)) # 缺了学习率调度器
+        trainer = Trainer(model=model,
+                          args=train_args,
+                          data_collator=mycollate, # 要么给自己的，要么在定义trainer后面单独写一个data_collator=None，不然代码里有默认collate
+                          train_dataset=dataset["train"],
+                          eval_dataset=dataset["eval"],
+                          tokenizer=tokenizer,
+                          optimizers=(optimizer, None)) # 缺了学习率调度器
 
-    trainer.train()
+        trainer.train()
 
+        model = trainer.model
 
-    # 2. 用基础模型预测unlabeled数据集
-    unlabeled_dataset = dataset["unlabeled"]
-    # unlabeled_dataset 还是没有标签的
+        # 2. 用基础模型预测unlabeled数据集
+        unlabeled_dataset = dataset["unlabeled"]
+        # 转一下
 
-    def give_x_y_score():
-        pass
+        selftrain_args = SelfTrainTrainer(output_dir=args.save_dir,
+                                          num_train_epochs=args.selftrain_iteration, # 这个指每个self_train里面的epoch
+                                          per_device_train_batch_size=args.batch_size,
+                                          save_steps=1000,
+                                          learning_rate=args.lr,
+                                          evaluation_strategy="epoch",
+                                          selftrain_topk=4)
 
-    # unlabeled_loader = DataLoader(unlabeled_dataset, batch_size=args.batch_size, collate_fn=self_train_collate)
+        self_trainer = SelfTrainTrainer(model=model,
+                                   args=selftrain_args,
+                                   data_collator=self_train_collate,
+                                   train_dataset=unlabeled_dataset,
+                                   tokenizer=tokenizer,
+                                   optimizers=(optimizer, None))
 
+        self_trainer.train()
 
-
+    # save
+    
 
